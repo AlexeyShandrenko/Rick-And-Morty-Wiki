1f7629eed910d8651c0a8689fe082fd25a1360bc
AlexeySnadrenko <adshandrenko@gmail.com>
2022-06-01 15:42:36 +0300

    new!

--- src/components/ui/logo/Logo.js
+++ src/components/ui/logo/Logo.js
@@ -6,11 +6,7 @@ const Logo = () => {
   return (
     <div className={styles.logo}>
       <Link to="/" className="logo__link">
-        <img
-          className={styles.logo__img}
-          src="./images/icons/logo.png"
-          alt=""
-        />
+        <img className={styles.logo__img} src="./images/icons/logo.png" />
       </Link>
     </div>
   );
